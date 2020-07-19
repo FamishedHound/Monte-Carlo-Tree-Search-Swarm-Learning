5d98e7ff4354489cd6b2c8fc8361db28c6df2245
Rhiannon Walmsley <kittivelae@riseup.net>
2020-07-19 14:47:44 +0100

    cherry picked and removed some redundant stuff in Node

--- src/pack_technical/Tree.java
+++ src/pack_technical/Tree.java
@@ -4,7 +4,9 @@ import pack_boids.BoidGeneric;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Tree {
     //root.depth is always 0
